f7fdd2f2792d3f8d2294594fb355194e25cef7eb
Alex Palomes <alejandropalomes@hotmail.com>
2020-04-23 04:19:22 +0200

    new js operations

--- assets/script/script2.js
+++ assets/script/script2.js
@@ -1,50 +1,78 @@
 document.querySelector(".btn__container").addEventListener("click", buttonClick);
 
+let operatorsArray = [];
+let operatorsBg = [];
+document.querySelectorAll(".btn__light").forEach(element => {
+    operatorsArray.push(element.getAttribute("value"));
+    operatorsBg.push(getComputedStyle(element).backgroundColor);
+});
+console.log(operatorsArray)
+console.log(operatorsBg)
+let screen = document.querySelector(".display");
+screen.innerHTML = "";
+let firstValue;
+let secondValue;
 
 function buttonClick(event) {
     let button = event.target;
 
     if (button.matches(".btn")) {
+        buttonBackground = button;
         button = button.getAttribute("value");
-        console.log(button);
-        addToScreen(button);
+        colorBg = getComputedStyle(document.querySelector(".calculator__container")).backgroundColor;
 
+        if (operatorsArray.includes(button)) {
+            firstValue = screen.innerHTML;
+            activeBg = buttonBackground;
+            document.querySelectorAll(".btn__light").forEach(element => {
+                element.style.backgroundColor = colorBg;
+            });
+            screen.innerHTML = "";
+            activeOperator = button;
+            buttonBackground.style.backgroundColor = "red";
 
-        switch (button) {
-            case "9":
-                console.log("Fuck it nigga");
-                break;
+        } else if (button === "=") {
+            secondValue = screen.innerHTML;
+            activeBg.style.backgroundColor = colorBg;
+            obtainResult(firstValue, secondValue, activeOperator);
 
-            default:
-                break;
+        } else if (button === "c") {
+            screen.innerHTML = "";
+            activeBg.style.backgroundColor = colorBg;
+
+        } else if (button === "*(-1)") {
+            screen.innerHTML = parseFloat(screen.innerHTML) * -1;
+        } else {
+            addToScreen(button);
         }
-
-
     }
 }
 
-
-function addToScreen(value){
-    console.log("yep")
-    let screen = document.querySelector(".display");
-    // screen += value;
+function addToScreen(value) {
     screen.innerHTML += value;
 }
 
+function obtainResult(a, b, operator) {
+    result = a + operator + b;
+    screen.innerHTML = eval(result);
+}
 
-// ---------- 👻 DARK MODE 👻  ------------ \\
-
+//! ---------- 👻 DARK MODE 👻  ------------ !\\
 
 document.querySelector(".switch").addEventListener("click", () => {
 
-    //document.querySelector(".testeo").classList.toggle("transition");
+    // document.querySelector(".testeo").classList.toggle("transition");
     document.querySelector(".slider").classList.toggle("sliderActive");
     document.querySelector(".slider").classList.toggle("sliderActive:before");
     document.querySelector(".top__container").classList.toggle("darkMode");
     document.querySelector(".btn__container").classList.toggle("darkMode2");
     document.querySelector(".calculator__container").classList.toggle("darkModeText");
+    document.querySelector(".calculator__container").classList.toggle("darkMode");
 
     document.querySelectorAll(".btn__light").forEach(element => {
         element.classList.toggle("darkMode");
+        if (element.style.backgroundColor != "red") {
+            element.style.backgroundColor = getComputedStyle(document.querySelector(".calculator__container")).backgroundColor;
+        }
     });
 })
