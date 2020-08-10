8f5f795ba13367a35028d00c5e2ef416df266586
beron <bleink@outlook.es>
2020-08-10 19:21:58 +0200

    calculator to make regular operations

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,19 +5,19 @@ function pressButton() {}
 var container = document.getElementById('buttons')
 
 
-//Variable para pantalla
+//Variable para screen
 
-var pantalla = document.querySelector(".display");
+var screen = document.querySelector(".display");
 
 //Event Listener para botones de numeros y operaciones:
 // console.log(container);
+var operandos = ["+", "*", "/", "-", "%"]
 container.addEventListener("click", (event) => {
-    let operandos = ["+", "*", "/", "-", "%"]
     if (event.target.matches(".operation")) {
-        let text = pantalla.innerText;
-        console.log((text.length>0)?1:2);
+        let text = screen.innerText;
+        console.log((text.length > 0) ? 1 : 2);
         if (event.target.getAttribute("value") == "*(-1)") {
-            if(text.length>0){
+            if (text.length > 0) {
                 let i = text.length - 1;
                 console.log(text)
                 while ((!operandos.includes(text[i])) && i > 0) {
@@ -27,7 +27,8 @@ container.addEventListener("click", (event) => {
                 if (i == 0) {
                     console.log(text);
                     text = "(-" + text + ")";
-                    pantalla.innerText = text;
+                    screen.innerText = text;
+                    showText();
                 } else {
                     let finalText = "";
                     for (let contador = 0; contador < i + 1; contador++) {
@@ -39,39 +40,53 @@ container.addEventListener("click", (event) => {
                     }
                     num1 += ")"
                     finalText += num1;
-                    pantalla.innerText = finalText;
-
+                    screen.innerText = finalText;
+                    showText();
                 }
             }
-        }  
-        else if(operandos.includes(event.target.getAttribute("value"))){
-                if(!operandos.includes(pantalla.innerText[pantalla.innerText.length - 1]) && text.length>0){
+        } else if (operandos.includes(event.target.getAttribute("value"))) {
+            if (!operandos.includes(screen.innerText[screen.innerText.length - 1]) && text.length > 0) {
                 //console.log("que dolor");
-                pantalla.innerText += event.target.getAttribute("value")
+                screen.innerText += event.target.getAttribute("value")
+                showText();
             }
-        }else if((pantalla.innerText[pantalla.innerText.length - 1] !== ")")) {
-            pantalla.innerText += event.target.getAttribute("value")
+        } else if ((screen.innerText[screen.innerText.length - 1] !== ")")) {
+            screen.innerText += event.target.getAttribute("value")
+            showText();
         }
-        
-        //  else if( (operandos.includes(event.target.getAttribute("value"))) &&
-        //     (!operandos.includes(pantalla.innerText[pantalla.innerText.length - 1])) ){
-        //         console.log("que dolor");
-        //         pantalla.innerText += event.target.getAttribute("value")
-        //     }
-        // else if (!operandos.includes(event.target.getAttribute("value"))&&
-        //     (pantalla.innerText[pantalla.innerText.length - 1] !== ")")) {
-        //         console.log(!operandos.includes(event.target.getAttribute("value"))?"a":"f")
-        //         console.log(!operandos.includes(pantalla.innerText[pantalla.innerText.length - 1])?"a":"f")
-        //         pantalla.innerText += event.target.getAttribute("value")
-        //     }
-        // else if (!((operandos.includes(event.target.getAttribute("value"))) &&
-        //         (operandos.includes(text[text.length-1])))){
-        //             console.log(!operandos.includes(event.target.getAttribute("value"))?"a":"f")
-        //             console.log(!operandos.includes(text[text.length - 1])?"a":"f")
-        //             pantalla.innerText += event.target.getAttribute("value")
-        //     }
-        
-   }
+
+        if (event.target.getAttribute("value") == ".") {
+            console.log("this shit works")
+            console.log(text.length)
+            if (text.length > 0) {
+                let i = text.length - 1;
+                console.log(text)
+                while ((!operandos.includes(text[i])) && i > 0) {
+                    i--;
+                }
+                console.log(i);
+                if (i == 0) {
+                    console.log(text);
+                    text = "0.";
+                    screen.innerText = text;
+                    showText();
+                } else {
+                    let finalText = "";
+                    for (let contador = 0; contador < i + 1; contador++) {
+                        finalText += text[contador];
+                    }
+                    let num1 = "(-";
+                    for (let contador = i + 1; contador < text.length; contador++) {
+                        num1 += text[contador];
+                    }
+                    num1 += ")"
+                    finalText += num1;
+                    screen.innerText = finalText;
+                    showText();
+                }
+            }
+        }
+    }
 })
 
 //Event Listener para boton equals
@@ -79,25 +94,74 @@ container.addEventListener("click", (event) => {
 var equals = document.getElementById('equal')
 
 equals.addEventListener("click", function () {
-    console.log(pantalla.innerText)
-    pantalla.innerText = (eval(pantalla.innerText)).toFixed(2)
-    console.log(pantalla.innerText)
+    console.log(screen.innerText)
+    screen.innerText = (eval(screen.innerText)).toFixed(2)
+    let container = document.querySelector(".display__visible");
+    let text = screen.innerText;
+    let textVisible = "";
+    let i = text.length;
+    if( i > 9 ){
+        textVisible = text.substring(0,4) + "x10^" + (i - 5)
+        container.innerText = textVisible;
+    }
+    else{
+        container.innerText = text;
+    }
+    console.log(screen.innerText)
 })
 
 // Event Listener para clear button
 
 var clear = document.getElementById('clear')
 clear.addEventListener("click", (event) => {
-    pantalla.innerText = ""
+    screen.innerText = ""
+    showText();
 })
 
-function checkDoubleOperator(){
-    
+// EventListener para un solo punto por número
+
+var dot = document.getElementById("dot")
+dot.addEventListener("click", () => {
+    let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    let text = screen.innerText;
+    let i = screen.innerText.length - 1;
+    let caracter = "1";
+    if (!operandos.includes(text[i])) {
+        while (caracter != "." && caracter!=")") {
+            caracter = text[i];
+            if (operandos.includes(caracter) || i == 0) {
+                screen.innerText+=".";
+                showText();
+                break;
+            }
+            i--;
+        }
+    }
+})
+
+// Screen to Show
+
+function showText(){
+    let text = screen.innerText;
+    let textVisible="";
+    let textContainer = document.querySelector(".display__visible")
+    let i = text.length;
+    if(i>10){
+        let countCharVisible = i - 9;
+        while(countCharVisible < i){
+            textVisible += text[countCharVisible];
+            countCharVisible++;
+        }
+        textContainer.innerText = textVisible;
+        console.log(textContainer);
+    }
+    else{
+        textContainer.innerText = text;
+        console.log(textContainer);
+    }
 }
 
-
-
-// ---------- 👻 DARK MODE 👻  ------------ \\
+//! ---------- 👻 DARK MODE 👻  ------------ !\\
 
 
 document.querySelector(".switch").addEventListener("click", () => {
@@ -108,6 +172,7 @@ document.querySelector(".switch").addEventListener("click", () => {
     document.querySelector(".top__container").classList.toggle("darkMode");
     document.querySelector(".btn__container").classList.toggle("darkMode2");
     document.querySelector(".calculator__container").classList.toggle("darkModeText");
+    document.querySelector("body").classList.toggle("darkBodyBackground");
 
     document.querySelectorAll(".btn__light").forEach(element => {
         element.classList.toggle("darkMode");
@@ -127,7 +192,7 @@ else {
         function a(){
             console.log(!operandos.includes(event.target.getAttribute("value"))?"a":"f")
             console.log(!operandos.includes(text[text.length - 1])?"a":"f")
-            pantalla.innerText += event.target.getAttribute("value")
+            screen.innerText += event.target.getAttribute("value")
         }*/
 
 
--- assets/script/script2.js
+++ assets/script/script2.js
@@ -1,78 +1,166 @@
-document.querySelector(".btn__container").addEventListener("click", buttonClick);
+function pressButton() {}
 
-let operatorsArray = [];
-let operatorsBg = [];
-document.querySelectorAll(".btn__light").forEach(element => {
-    operatorsArray.push(element.getAttribute("value"));
-    operatorsBg.push(getComputedStyle(element).backgroundColor);
-});
-console.log(operatorsArray)
-console.log(operatorsBg)
-let screen = document.querySelector(".display");
-screen.innerHTML = "";
-let firstValue;
-let secondValue;
+//Variable para botones
 
-function buttonClick(event) {
-    let button = event.target;
+var container = document.getElementById('buttons')
 
-    if (button.matches(".btn")) {
-        buttonBackground = button;
-        button = button.getAttribute("value");
-        colorBg = getComputedStyle(document.querySelector(".calculator__container")).backgroundColor;
 
-        if (operatorsArray.includes(button)) {
-            firstValue = screen.innerHTML;
-            activeBg = buttonBackground;
-            document.querySelectorAll(".btn__light").forEach(element => {
-                element.style.backgroundColor = colorBg;
-            });
-            screen.innerHTML = "";
-            activeOperator = button;
-            buttonBackground.style.backgroundColor = "red";
+//Variable para screen
 
-        } else if (button === "=") {
-            secondValue = screen.innerHTML;
-            activeBg.style.backgroundColor = colorBg;
-            obtainResult(firstValue, secondValue, activeOperator);
+var screen = document.querySelector(".display");
 
-        } else if (button === "c") {
-            screen.innerHTML = "";
-            activeBg.style.backgroundColor = colorBg;
+//Event Listener para botones de numeros y operaciones:
+// console.log(container);
+var operandos = ["+", "*", "/", "-", "%"]
+container.addEventListener("click", (event) => {
+    if (event.target.matches(".operation")) {
+        let text = screen.innerText;
+        console.log((text.length > 0) ? 1 : 2);
+        if (event.target.getAttribute("value") == "*(-1)") {
+            if (text.length > 0) {
+                let i = text.length - 1;
+                console.log(text)
+                while ((!operandos.includes(text[i])) && i > 0) {
+                    i--;
+                }
+                console.log(i);
+                if (i == 0) {
+                    console.log(text);
+                    text = "(-" + text + ")";
+                    screen.innerText = text;
+                } else {
+                    let finalText = "";
+                    for (let contador = 0; contador < i + 1; contador++) {
+                        finalText += text[contador];
+                    }
+                    let num1 = "(-";
+                    for (let contador = i + 1; contador < text.length; contador++) {
+                        num1 += text[contador];
+                    }
+                    num1 += ")"
+                    finalText += num1;
+                    screen.innerText = finalText;
+                }
+            }
+        } else if (operandos.includes(event.target.getAttribute("value"))) {
+            if (!operandos.includes(screen.innerText[screen.innerText.length - 1]) && text.length > 0) {
+                //console.log("que dolor");
+                screen.innerText += event.target.getAttribute("value")
+            }
+        } else if ((screen.innerText[screen.innerText.length - 1] !== ")")) {
+            screen.innerText += event.target.getAttribute("value")
+        }
 
-        } else if (button === "*(-1)") {
-            screen.innerHTML = parseFloat(screen.innerHTML) * -1;
-        } else {
-            addToScreen(button);
+        if (event.target.getAttribute("value") == ".") {
+            console.log("this shit works")
+            console.log(text.length)
+            if (text.length > 0) {
+                let i = text.length - 1;
+                console.log(text)
+                while ((!operandos.includes(text[i])) && i > 0) {
+                    i--;
+                }
+                console.log(i);
+                if (i == 0) {
+                    console.log(text);
+                    text = "0.";
+                    screen.innerText = text;
+                } else {
+                    let finalText = "";
+                    for (let contador = 0; contador < i + 1; contador++) {
+                        finalText += text[contador];
+                    }
+                    let num1 = "(-";
+                    for (let contador = i + 1; contador < text.length; contador++) {
+                        num1 += text[contador];
+                    }
+                    num1 += ")"
+                    finalText += num1;
+                    screen.innerText = finalText;
+                }
+            }
         }
     }
-}
+})
 
-function addToScreen(value) {
-    screen.innerHTML += value;
-}
+//Event Listener para boton equals
 
-function obtainResult(a, b, operator) {
-    result = a + operator + b;
-    screen.innerHTML = eval(result);
+var equals = document.getElementById('equal')
+
+equals.addEventListener("click", function () {
+    console.log(screen.innerText)
+    screen.innerText = (eval(screen.innerText)).toFixed(2)
+    console.log(screen.innerText)
+})
+
+// Event Listener para clear button
+
+var clear = document.getElementById('clear')
+clear.addEventListener("click", (event) => {
+    screen.innerText = ""
+})
+
+// EventListener para un solo punto por número
+
+var dot = document.getElementById("dot")
+dot.addEventListener("click", () => {
+    let nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+    let text = screen.innerText;
+    let i = screen.innerText.length - 1;
+    let caracter = "1";
+    if (!operandos.includes(text[i])) {
+        while (caracter != "." && caracter!=")") {
+            caracter = text[i];
+            if (operandos.includes(caracter) || i == 0) {
+                screen.innerText+=".";
+                break;
+            }
+            i--;
+        }
+    }
+})
+
+// Screen to Show
+                                                  
+let showText = screen.innerText;
+let realText = "";
+function showText(){
+    let showCount = showText
 }
 
 //! ---------- 👻 DARK MODE 👻  ------------ !\\
 
+
 document.querySelector(".switch").addEventListener("click", () => {
 
-    // document.querySelector(".testeo").classList.toggle("transition");
+    //document.querySelector(".testeo").classList.toggle("transition");
     document.querySelector(".slider").classList.toggle("sliderActive");
     document.querySelector(".slider").classList.toggle("sliderActive:before");
     document.querySelector(".top__container").classList.toggle("darkMode");
     document.querySelector(".btn__container").classList.toggle("darkMode2");
     document.querySelector(".calculator__container").classList.toggle("darkModeText");
-    document.querySelector(".calculator__container").classList.toggle("darkMode");
+    document.querySelector("body").classList.toggle("darkBodyBackground");
 
     document.querySelectorAll(".btn__light").forEach(element => {
         element.classList.toggle("darkMode");
-        if (element.style.backgroundColor != "red") {
-            element.style.backgroundColor = getComputedStyle(document.querySelector(".calculator__container")).backgroundColor;
-        }
     });
 })
+
+
+
+/*
+else {
+            (operandos.includes(event.target.getAttribute("value"))) &&
+            (operandos.includes(text[text.length-1]))?o():a();
+        } 
+        function o(){
+                console.log("que dolor");
+            }
+        function a(){
+            console.log(!operandos.includes(event.target.getAttribute("value"))?"a":"f")
+            console.log(!operandos.includes(text[text.length - 1])?"a":"f")
+            screen.innerText += event.target.getAttribute("value")
+        }*/
+
+
+// ////////// OBSOLETO \\\\\\\\\\\ \\
